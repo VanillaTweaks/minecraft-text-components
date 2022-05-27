216f961d87731edb8e8c6e90bf1887ea7ef23b57
Grant Gryczan <grantgryczan@gmail.com>
2022-05-27 14:41:56 -0400

    Implement more alignment functions

---
+++ src/alignment.py
@@ -0,0 +1,34 @@
+from .advances.get_advance import get_advance
+from .container import container
+from .pad_each_line import pad_each_line
+from .types import TextComponent
+
+
+def center(component: TextComponent):
+    """Centers a text component, automatically minified."""
+
+    return pad_each_line(component, lambda advance: (container.width - advance) / 2)
+
+
+def right(component: TextComponent):
+    """Right-aligns a text component, automatically minified."""
+
+    return pad_each_line(component, lambda advance: container.width - advance)
+
+
+def local_center(component: TextComponent):
+    """Centers a text component using its own width as the container width,
+    automatically minified.
+    """
+
+    with container(get_advance(component)):
+        return center(component)
+
+
+def local_right(component: TextComponent):
+    """Right-aligns a text component using its own width as the container width,
+    automatically minified.
+    """
+
+    with container(get_advance(component)):
+        return right(component)
--- src/center.py
+++
@@ -1,7 +0,0 @@
-from .container import container
-from .pad_each_line import pad_each_line
-from .types import TextComponent
-
-
-def center(component: TextComponent):
-    return pad_each_line(component, lambda advance: (container.width - advance) / 2)
