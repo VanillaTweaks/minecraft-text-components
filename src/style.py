aceef4937fc1d89e845674c440dde2f949a16f71
Grant Gryczan <grantgryczan@gmail.com>
2022-05-27 14:34:26 -0400

    Implement `style`

---
+++ src/style.py
@@ -0,0 +1,18 @@
+from typing_extensions import Unpack
+
+from .minify.minify import minify
+from .types import TextComponent, TextComponentFormatting
+
+
+def style(
+    component: TextComponent,
+    **formatting: Unpack[TextComponentFormatting],
+):
+    """Makes the specified text component inherit the specified formatting,
+    automatically minified.
+    """
+
+    if formatting is None:
+        formatting = {}
+
+    return minify({"text": "", **formatting, "extra": [component]})  # type: ignore
