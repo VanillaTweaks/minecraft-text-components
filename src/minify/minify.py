029b0782dd410533e205b8cec2996bc478cb3a4f
Grant Gryczan <grantgryczan@gmail.com>
2022-05-27 00:44:39 -0400

    Stub for `minify`

---
+++ src/minify/minify.py
@@ -0,0 +1,9 @@
+from ..types import TextComponent
+
+
+def minify(component: TextComponent) -> TextComponent:
+    """Transforms a text component to be as short and simplified as possible without
+    changing its in-game appearance.
+    """
+
+    ...
